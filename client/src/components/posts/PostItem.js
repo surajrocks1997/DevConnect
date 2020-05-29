a70c8b667473f87d8930ef2245a26c4e055b4b19
surajrocks1997 <surajrocks1997@gmail.com>
2020-05-29 13:10:39 +0530

    Like and unlike functionality added

--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -4,7 +4,11 @@ import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import Moment from "react-moment";
 
+import { addLike, removeLike } from "../../actions/post";
+
 const PostItem = ({
+  addLike,
+  removeLike,
   auth,
   post: { _id, text, name, avatar, user, likes, comments, date },
 }) => {
@@ -21,19 +25,31 @@ const PostItem = ({
         <p className="post-date">
           Posted on <Moment format="YYYY/MM/DD">{date}</Moment>
         </p>
-        <button type="button" className="btn btn-light">
-          <i className="fas fa-thumbs-up"></i>{' '}
+
+        <button
+          onClick={(e) => addLike(_id)}
+          type="button"
+          className="btn btn-light"
+        >
+          <i className="fas fa-thumbs-up"></i>{" "}
           <span>{likes.length > 0 && <span>{likes.length}</span>}</span>
         </button>
-        <button type="button" className="btn btn-light">
+
+        <button
+          onClick={(e) => removeLike(_id)}
+          type="button"
+          className="btn btn-light"
+        >
           <i className="fas fa-thumbs-down"></i>
         </button>
+
         <Link to={`/post/${_id}`} className="btn btn-primary">
           Discussion{" "}
           {comments.length > 0 && (
             <span className="comment-count">{comments.length}</span>
           )}
         </Link>
+
         {!auth.loading && user === auth.user._id && (
           <button type="button" className="btn btn-danger">
             <i className="fas fa-times"></i>
@@ -53,4 +69,4 @@ const mapStateToProps = (state) => ({
   auth: state.auth,
 });
 
-export default connect(mapStateToProps, {})(PostItem);
+export default connect(mapStateToProps, { addLike, removeLike })(PostItem);
